fe83b849dfbd1e87b6e9ebf4d52e0f5c302979e6
Aiman-27 <74717048+Aiman-27@users.noreply.github.com>
2021-06-21 10:07:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,40 @@ function prepareFood(callback){
     setTimeout(()=>{
         console.log("Prepare food");
         callback("Food is Ready");
+    },10);
+    
+}
+function prepareFrenchToast(callback){
+    setTimeout(()=>{
+        console.log("Prepare French Toast");
+        callback("French Toast is Ready");
     },20);
     
 }
 
-function mycallback(value){
-    console.log("Value = ",value);
+function prepareCoffee(callback){
+    setTimeout(()=>{
+        console.log("Prepare Coffe");
+        callback("Coffe is Ready");
+    },30);
+    
 }
 
+function mycallback(value){
+    console.log("Food is Ready callback = ",value);
+    prepareFrenchToast(frenchToastcallback);
+}
+
+function frenchToastcallback(value){
+    console.log("French Toast is Ready callback = ",value);
+    prepareFood(coffeecallback);
+}
+
+function coffeecallback(value){
+    console.log("Coffe is Ready callback = ",value);
+}
 
 prepareFood(mycallback);
+
 
 console.log("After Prepare Food dinner is ready");
